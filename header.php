468b32d47e43ca0e08a499e4193481c4b5af45e1
Lucas Filgueiras <lucasflacko@gmail.com>
2023-09-08 15:57:16 -0300

    chores/bootstrap

--- header.php
+++ header.php
@@ -15,7 +15,13 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class( ); ?> >
-    <?php wp_body_open(); ?>
+
+    <?php 
+        if(function_exists('wp_body_open')){
+            wp_body_open();
+        }
+    ?>
+
     <header> 
         Header
     </header>
