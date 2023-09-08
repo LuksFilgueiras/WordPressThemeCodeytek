d5c6d1951d042310b2b09d531f3d14acb828850d
Lucas Filgueiras <lucasflacko@gmail.com>
2023-09-08 14:04:45 -0300

    feat/global style enqueued

--- functions.php
+++ functions.php
@@ -4,3 +4,13 @@
  * 
  * @package Aquila
  */
+
+ 
+
+function aquila_enqueue_scripts(){
+    wp_enqueue_style( 'style-global', get_template_directory_uri() . '/style.css', [], filemtime(get_template_directory() . '/style.css'), 'all');
+}
+
+add_action( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' );
+
+ ?>
