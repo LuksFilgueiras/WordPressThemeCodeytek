307a758fa2edfcb4ff67bb3a7c246a7ff1b8791d
Lucas Filgueiras <lucasflacko@gmail.com>
2023-09-12 10:58:00 -0300

    feat/the loop index.php

--- index.php
+++ index.php
@@ -8,9 +8,27 @@
      get_header();
 ?>
 
-    <div class="content">
-        Content Index
-    </div>
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <?php
+            if(have_posts(  )){
+                ?>
+                    <div class="container">
+                        <?php
+                            while(have_posts(  )) : the_post(  );
+                            ?>
+                              <h1> <?php the_title(); ?> </h1>
+                              <?php
+                                the_excerpt();
+                                the_content();
+                            endwhile;
+                        ?>
+                    </div>
+                <?php
+            }
+        ?>  
+    </main>
+</div>
 
 <?php 
     get_footer();
